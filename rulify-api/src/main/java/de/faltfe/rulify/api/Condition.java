00ae23a31a98527980ffcc15154da743a4514cc3
faltfe <13038138+faltfe@users.noreply.github.com>
2026-04-14 23:35:02 +0200

    docs: update javadoc of api package

--- rulify-api/src/main/java/de/faltfe/rulify/api/Condition.java
+++ rulify-api/src/main/java/de/faltfe/rulify/api/Condition.java
@@ -3,21 +3,26 @@ package de.faltfe.rulify.api;
 import java.util.function.Predicate;
 
 /**
- * Represents decoupled check of self defined conditions against any object.
+ * Represents a decoupled, self-defined condition check against any object.
  *
  * <p>The traditional way of writing checks was the <i>if-else</i> statement. The big downside of that is that these
- * statements are highly bounded to a class or method. In contrast to that a {@link Condition} is decoupled from any
- * related class, can be reused, is chainable and easy to test.
+ * statements are highly bound to a class or method. In contrast, a {@link Condition} is decoupled from any
+ * related class, can be reused, is chainable, and easy to test.
+ *
+ * <p>For conditions that need to modify data before evaluation, consider using {@link Modifier} in combination
+ * with other rule constructs. For performing actions based on conditions, see {@link Rule} and {@link Effect}.
  *
  * <pre>{@code
  * Condition<String> isEmpty = (s) -> s.isEmpty();
  * if (isEmpty.test("String to test")) {
- *     // String was not empty
+ *     // String is empty
  * }
  * }</pre>
  *
  * @param <T> the type of input to the check
  * @see Predicate
+ * @see Rule
+ * @see Effect
  */
 @FunctionalInterface
 public interface Condition<T> extends Predicate<T> {
