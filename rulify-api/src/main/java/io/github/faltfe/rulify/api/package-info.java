9c19d9b708d8a87e000967d769662c671e7c5070
faltfe <13038138+faltfe@users.noreply.github.com>
2026-06-02 14:01:14 +0200

    feat: rulify API is now defined as NonNull by default

---
+++ rulify-api/src/main/java/io/github/faltfe/rulify/api/package-info.java
@@ -0,0 +1,4 @@
+@org.jspecify.annotations.NullMarked
+package io.github.faltfe.rulify.api;
+
+import org.jspecify.annotations.NullMarked;
---
+++ rulify-api/src/main/java/io/github/faltfe/rulify/internal/package-info.java
@@ -0,0 +1,4 @@
+@org.jspecify.annotations.NullMarked
+package io.github.faltfe.rulify.internal;
+
+import org.jspecify.annotations.NullMarked;
